b2e2cae93c4255b917f2b1a6eb05dc81d8656759
linmingbing <linmingbing.123>
2020-04-03 15:33:23 +0800

    移动图层搜索地址

--- lib/testdemo/map_test.dart
+++ lib/testdemo/map_test.dart
@@ -18,6 +18,8 @@ import 'package:flutter_qyyim/view_model/place_view_model.dart';
 
 import 'cross_data/event_bus.dart';
 
+final _assetsIcon1 = Uri.parse('assets/images/wechat_locate.png');
+
 class MapTestPage extends StatefulWidget {
   @override
   _MapTestPageState createState() => new _MapTestPageState();
@@ -28,10 +30,15 @@ class _MapTestPageState extends State<MapTestPage> {
 
   String showAddressText = "周围的数据";
   ScrollController sC;
+  List<Marker> _markers = [];
+
+  var index = 0;
+
+  PlaceViewModle placeViewModel;
+
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     sC = new ScrollController();
   }
@@ -39,9 +46,11 @@ class _MapTestPageState extends State<MapTestPage> {
   @override
   void dispose() {
     // TODO: implement dispose
+    _controller.dispose();
     super.dispose();
   }
 
+  /// 套上ProviderWidget包装不过渡刷新
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -52,31 +61,26 @@ class _MapTestPageState extends State<MapTestPage> {
               child: AmapView(
                 mapType: MapType.Standard,
                 showZoomControl: false,
+                onMapMoveEnd: (mapMove) async {
+                  await onMapMoveEnd(context);
+                  final latLng = await _controller?.getCenterCoordinate();
+                  if (latLng != null) {
+                    placeViewModel?.getCurrentLocation(latLng);
+                  }
+                },
                 maskDelay: Duration(milliseconds: 500),
                 onMapCreated: (controller) async {
                   _controller = controller;
                   if (await requestPermission()) {
-                    await controller.setZoomLevel(16);
                     await controller
                         .showMyLocation(MyLocationOption(show: true));
-
-                    /*final marker = await _controller?.addMarker(
-                      MarkerOption(
-                        latLng: LatLng(22.53233,113.952584),
-                        title: '北京',
-                        snippet: '描述',
-                        iconUri: Uri.parse('images/test_icon.png'),
-                        imageConfig: createLocalImageConfiguration(context),
-                        width: 48,
-                        height: 48,
-                        object: '自定义数据',
-                      ),
-                    );
-                    _markers.add(marker);*/
-                    setState(() {});
+                    await controller.setZoomLevel(16);
+                    setState(() {
+                    });
                   } else {
                     ToastUtils.show("open your map permission", context);
                   }
+
                 },
               ),
               height: 455,
@@ -112,44 +116,69 @@ class _MapTestPageState extends State<MapTestPage> {
           ]),
           (_controller != null)
               ? Flexible(
-                  child: Column(
-                    children: <Widget>[
-                      //Text('$showAddressText'),
-                      SearchBar(
-                        searchBarType: SearchBarType.homeLight,
-                        defaultText: '搜索地址',
-                        leftButtonClick: () {},
-                      ),
-                      ProviderWidget<PlaceViewModle>(
-                        model: new PlaceViewModle(),
-                        onModelReady: (modle) async {
-                          final latLng = await _controller?.getLocation();
-                          ToastUtils.show(
-                              '当前经纬度Extension: ${latLng.toString()}', context);
-                          modle.getCurrentLocation(latLng);
+            child: Column(
+              children: <Widget>[
+                //Text('$showAddressText'),
+                Text('$index'),
+                SearchBar(
+                  searchBarType: SearchBarType.homeLight,
+                  defaultText: '搜索地址',
+                  leftButtonClick: () {},
+                ),
+                ProviderWidget<PlaceViewModle>(
+                  model: PlaceViewModle(),
+                  onModelReady: (modle) async {
+                    placeViewModel = modle;
+                    final latLng = await _controller?.getCenterCoordinate();
+                    if (latLng != null) {
+                      modle?.getCurrentLocation(latLng);
+                    }
+                  },
+                  builder: (context, modle, widget){
+                    return Flexible(
+                      child: new ListView.builder(
+                        controller: sC,
+                        shrinkWrap: true,
+                        padding: EdgeInsets.all(8.0),
+                        reverse: false,
+                        itemBuilder: (context, int index) {
+                          return new PlaceView(modle.poiTitleList[index]);
                         },
-                        builder: (context, modle, widget) {
-                          return Flexible(
-                            child: new ListView.builder(
-                              controller: sC,
-                              shrinkWrap: true,
-                              //内容适配
-                              padding: EdgeInsets.all(8.0),
-                              reverse: false,
-                              itemBuilder: (context, int index) {
-                                return new PlaceView(modle.poiTitleList[index]);
-                              },
-                              itemCount: modle.poiTitleList?.length,
-                            ),
-                          );
-                        },
+                        itemCount: modle.poiTitleList?.length,
                       ),
-                    ],
-                  ),
-                )
+                    );
+                  },
+                ),
+              ],
+            ),
+          )
               : Container(),
         ],
       ),
     );
+  }
+
+  Future onMapMoveEnd(BuildContext context) async {
+    final center = await _controller?.getCenterCoordinate();
+    if (_markers.isNotEmpty) {
+      await _markers[0].remove();
+      _markers.removeAt(0);
+    }
+    final marker = await _controller?.addMarker(
+      MarkerOption(
+        latLng: center,
+        title: '北京',
+        snippet: '描述',
+        iconUri: _assetsIcon1,
+        imageConfig: createLocalImageConfiguration(context),
+        width: 58,
+        height: 58,
+        object: '自定义数据',
+      ),
+    );
+    _markers.add(marker);
+
+
+    index++;
   }
 }
